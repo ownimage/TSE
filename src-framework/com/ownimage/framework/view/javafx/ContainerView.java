c671225ae12e048f9b12642cbe475f514f0fceba
ownimage <keith@ownimage.co.uk>
2018-07-25 19:17:12 +0100

    Issue #67 Control elements on screen (e.g Integer control) do not respect the isVisible status of their underlying control when created

--- src-framework/com/ownimage/framework/view/javafx/ContainerView.java
+++ src-framework/com/ownimage/framework/view/javafx/ContainerView.java
@@ -3,6 +3,7 @@ package com.ownimage.framework.view.javafx;
 import java.util.Iterator;
 
 import com.ownimage.framework.control.container.IContainer;
+import com.ownimage.framework.control.control.IControl;
 import com.ownimage.framework.control.layout.IViewable;
 
 import javafx.geometry.Pos;
@@ -13,33 +14,37 @@ import javafx.scene.layout.VBox;
 
 public class ContainerView extends ViewBase<IContainer> {
 
-	private VBox mUI;
+    private VBox mUI;
 
-	public ContainerView(final IContainer pContainer) {
-		super(pContainer);
-		createView();
-	}
+    public ContainerView(final IContainer pContainer) {
+        super(pContainer);
+        createView();
+    }
 
-	private void createView() {
-		mUI = new VBox();
+    private void createView() {
+        mUI = new VBox();
 
-		Iterator<IViewable<?>> children = mControl.getViewableChildrenIterator();
-		while (children.hasNext()) {
-			IViewable<?> child = children.next();
+        Iterator<IViewable<?>> children = mControl.getViewableChildrenIterator();
+        while (children.hasNext()) {
+            IViewable<?> child = children.next();
 
-			Node childView = ((FXView) (child.createView())).getUI();
+            Node childView = ((FXView) (child.createView())).getUI();
+            if (child instanceof IControl) {
+                childView.setVisible(((IControl) child).isVisible()); // TODO these should align with ViewBase
+                childView.setManaged(((IControl) child).isVisible());
+            }
 
-			HBox hbox = new HBox();
-			hbox.setAlignment(Pos.TOP_LEFT);
-			hbox.getChildren().addAll(childView);
+            HBox hbox = new HBox();
+            hbox.setAlignment(Pos.TOP_LEFT);
+            hbox.getChildren().addAll(childView);
 
-			mUI.getChildren().add(hbox);
-		}
-	}
+            mUI.getChildren().add(hbox);
+        }
+    }
 
-	@Override
-	public Pane getUI() {
-		return mUI;
-	}
+    @Override
+    public Pane getUI() {
+        return mUI;
+    }
 
 }
