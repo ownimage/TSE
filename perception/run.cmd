0e5ec32c4214b75a32fddec8d5b99c8cca055d3b
ownimage <keith@ownimage.co.uk>
2019-09-03 08:09:58 +0100

    Issue-155 Change NY to Chester image

--- perception/run.cmd
+++ perception/run.cmd
@@ -1,1 +1,1 @@
-mvn exec:java -Dexec.mainClass="com.ownimage.perception.view.javafx.app.FXPerception" -Dexec.args="NY2.jpg"
+mvn exec:java -Dexec.mainClass="com.ownimage.perception.view.javafx.app.FXPerception" -Dexec.args="Chester.jpg"
