4f46741c2316855addc440a45ddd54d39ce465a8
ownimage <keith@ownimage.co.uk>
2019-12-31 11:47:26 +0000

    Issue-179 Canny Edge changes to default sizes

--- perception/src/main/java/com/ownimage/perception/transform/CannyEdgeTransform.java
+++ perception/src/main/java/com/ownimage/perception/transform/CannyEdgeTransform.java
@@ -81,7 +81,7 @@ public class CannyEdgeTransform extends BaseTransform implements IPixelMapTransf
             new BooleanControl("Show Lines", "showLines", getContainer(), true);
 
     private final ObjectControl<LineEndShape> mLineEndShape =
-            new ObjectControl<>("Line End Shape", "lineEndShape", getContainer(), LineEndShape.Straight, LineEndShape.values());
+            new ObjectControl<>("Line End Shape", "lineEndShape", getContainer(), LineEndShape.Curved, LineEndShape.values());
 
     private final ObjectControl<LineEndLengthType> mLineEndLengthType =
             new ObjectControl<>(
@@ -105,16 +105,16 @@ public class CannyEdgeTransform extends BaseTransform implements IPixelMapTransf
     private final IntegerControl mLongLineLength =
             new IntegerControl("Long Line Length", "longLineLength", getContainer(), 50, 1, 500, 10);
     private final DoubleControl mLongLineThickness =
-            new DoubleControl("Long Line Thickness", "longLineThickness", getContainer(), 3.0d, 0.0d, 10.0d);
+            new DoubleControl("Long Line Thickness", "longLineThickness", getContainer(), 1.5d, 0.0d, 10.0d);
     private final IntegerControl mMediumLineLength =
             new IntegerControl("Medium Line Length", "mediumLineLength", getContainer(), 0, 0, 1000, 20);
     private final DoubleControl mMediumLineThickness =
-            new DoubleControl("Medium Line Thickness", "mediumLineThickness", getContainer(), 2.0d, 0.0d, 10.0d);
+            new DoubleControl("Medium Line Thickness", "mediumLineThickness", getContainer(), 1.0d, 0.0d, 10.0d);
 
     private final IntegerControl mShortLineLength =
             new IntegerControl("Short Line Length", "shortLineLength", getContainer(), 0, 0, 1000, 20);
     private final DoubleControl mShortLineThickness =
-            new DoubleControl("Short Line Thickness", "shortLineThickness", getContainer(), 1.0d, 0.0d, 10.0d);
+            new DoubleControl("Short Line Thickness", "shortLineThickness", getContainer(), 0.5, 0.0d, 10.0d);
 
     private final ObjectControl<EqualizeValues> mEqualize =
             new ObjectControl<>(
